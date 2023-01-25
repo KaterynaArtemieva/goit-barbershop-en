7504600732b72d4fb45b8376de583c6573a70a16
Kateryna Artemieva <moruga.katya@gmail.com>
2023-01-25 17:29:41 +0000

    Підключила свайпер

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,12 @@
-var mySwiper = new Swiper(".swiper-container", {
-  direction: "vertical",
-  loop: true,
-  pagination: ".swiper-pagination",
-  grabCursor: true,
-  speed: 1000,
-  paginationClickable: true,
-  parallax: true,
-  autoplay: false,
-  effect: "slide",
-  mousewheelControl: 1
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
